bf85ac85c351e35cee40922cf57334ff03bc21e7
curran <curran.kelleher@gmail.com>
2016-09-23 12:46:34 +0530

    Add initial test

---
+++ test.js
@@ -0,0 +1,43 @@
+var assert = require("assert");
+var parse = require(".index");
+
+describe("json-template", function() {
+  it("should compute template for a string", function() {
+    var template = parse("{{foo}}");
+    assert.equal(template({ foo: "bar" }), "bar");
+    assert.deepEqual(template.parameters, [{ key: "foo" }]);
+  });
+});
+
+// "{{}}"
+// "}}{{"
+// "}}foo{{"
+
+//
+//var template = parse({ "PersonName": "{{person}}" });
+//expect(template.parameters).to.equal(
+//  [
+//    {
+//      key: "person",
+//      path: ["PersonName"]
+//    }
+//  ]
+//);
+//expect(template({ person: "Susanna" })).to.equal(
+//  { "PersonName": "Susanna" }
+//);
+//
+//{ "PersonName": "{{person:Bob}}" }
+//
+//[
+//  {
+//    key: "person",
+//    defaultValue: "Bob",
+//    path: ["PersonName"]
+//  }
+//]
+//
+//{ "gte": "{{startDate:now-24h}}" }
+//{ "index": "{{index}}" }
+
+
