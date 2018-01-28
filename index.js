d79e64acea87ed16beefcb24acf88c9a54e26fc5
javierblancosp <javicuba11@gmail.com>
2018-01-28 14:19:21 +0100

    :gift: issue #17

--- index.js
+++ index.js
@@ -3,11 +3,10 @@
 //
 // By Curran Kelleher and Chrostophe Serafin
 // November 2016
-var objectPath = require("object-path");
-var dedupe = require("dedupe");
+var objectPath = require('object-path');
+var dedupe = require('dedupe');
 
 module.exports = parse;
-
 
 // Parses the given template object.
 //
@@ -17,69 +16,64 @@ module.exports = parse;
 // The returned function has a `parameters` property,
 // which is an array of parameter descriptor objects,
 // each of which has a `key` property and possibly a `defaultValue` property.
-function parse(value){
-  switch(type(value)) {
-    case "string":
+function parse(value) {
+  switch (type(value)) {
+    case 'string':
       return parseString(value);
-    case "object":
+    case 'object':
       return parseObject(value);
-    case "array":
+    case 'array':
       return parseArray(value);
     default:
-      return Template(function (){ return value; }, []);
+      return Template(function() {
+        return value;
+      }, []);
   }
-};
-
-
-// An enhanced version of `typeof` that handles arrays and dates as well.
-function type(value){
-  return (
-    Array.isArray(value) ? "array" :
-    value instanceof Date ? "date" :
-    typeof value
-  );
 }
 
+// An enhanced version of `typeof` that handles arrays and dates as well.
+function type(value) {
+  return Array.isArray(value) ? 'array' : value instanceof Date ? 'date' : typeof value;
+}
 
 // Parses leaf nodes of the template object that are strings.
 // Also used for parsing keys that contain templates.
-var parseString = (function (){
-
+var parseString = (function() {
   // This regular expression detects instances of the
   // template parameter syntax such as {{foo}} or {{foo:someDefault}}.
   var regex = /{{(\w|:|[\s-+.,@/\//()?=*_])+}}/g;
 
-  return function (str){
-    if(regex.test(str)){
+  return function(str) {
+    if (regex.test(str)) {
+      var matches = str.match(regex), parameters = matches.map(Parameter);
 
-      var matches = str.match(regex),
-          parameters = matches.map(Parameter);
-
-      return Template(function (context){
+      return Template(function(context) {
         context = context || {};
-        return matches.reduce(function (str, match, i){
+        return matches.reduce(function(str, match, i) {
           var parameter = parameters[i];
           var value = objectPath.get(context, parameter.key) || parameter.defaultValue;
-          return str.replace(match, value);
+          if (typeof match === 'object') {
+            return match;
+          } else {
+            return str.replace(match, value);
+          }
         }, str);
       }, parameters);
-
     } else {
-      return Template(function (){
+      return Template(function() {
         return str;
       }, []);
     }
   };
-}());
-
+})();
 
 // Constructs a parameter object from a match result.
 // e.g. "['{{foo}}']" --> { key: "foo" }
 // e.g. "['{{foo:bar}}']" --> { key: "foo", defaultValue: "bar" }
-function Parameter(match){
+function Parameter(match) {
   match = match.substr(2, match.length - 4).trim();
-  var i = match.indexOf(":");
-  if(i !== -1){
+  var i = match.indexOf(':');
+  if (i !== -1) {
     return {
       key: match.substr(0, i),
       defaultValue: match.substr(i + 1)
@@ -89,48 +83,49 @@ function Parameter(match){
   }
 }
 
-
 // Constructs a template function with deduped `parameters` property.
-function Template(fn, parameters){
-    // Paul Brewer Dec 2017 add deduplication call, use only key property to eliminate
-    fn.parameters = dedupe(parameters, function(item){ return item.key });
+function Template(fn, parameters) {
+  // Paul Brewer Dec 2017 add deduplication call, use only key property to eliminate
+  fn.parameters = dedupe(parameters, function(item) {
+    return item.key;
+  });
   return fn;
 }
 
-
 // Parses non-leaf-nodes in the template object that are objects.
-function parseObject(object){
-
-  var children = Object.keys(object).map(function (key){
+function parseObject(object) {
+  var children = Object.keys(object).map(function(key) {
     return {
       keyTemplate: parseString(key),
       valueTemplate: parse(object[key])
     };
   });
 
-  return Template(function (context){
-    return children.reduce(function (newObject, child){
-      newObject[child.keyTemplate(context)] = child.valueTemplate(context);
-      return newObject;
-    }, {});
-  }, children.reduce(function (parameters, child){
+  return Template(
+    function(context) {
+      return children.reduce(function(newObject, child) {
+        newObject[child.keyTemplate(context)] = child.valueTemplate(context);
+        return newObject;
+      }, {});
+    },
+    children.reduce(function(parameters, child) {
       return parameters.concat(child.valueTemplate.parameters, child.keyTemplate.parameters);
-  }, []));
-
+    }, [])
+  );
 }
 
-
 // Parses non-leaf-nodes in the template object that are arrays.
-function parseArray(array){
-
+function parseArray(array) {
   var templates = array.map(parse);
 
-  return Template(function (context){
-    return templates.map(function (template){
-      return template(context);
-    });
-  }, templates.reduce(function (parameters, template){
-    return parameters.concat(template.parameters);
-  }, []));
-
+  return Template(
+    function(context) {
+      return templates.map(function(template) {
+        return template(context);
+      });
+    },
+    templates.reduce(function(parameters, template) {
+      return parameters.concat(template.parameters);
+    }, [])
+  );
 }
